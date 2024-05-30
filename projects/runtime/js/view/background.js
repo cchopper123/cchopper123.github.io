32a2edb550ff8b025b471fabc41186ab3a4dcefb
cchopper123 <c34504899@gmail.com>
2024-05-30 20:52:41 +0000

    saving

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -39,12 +39,13 @@ var background = function (window) {
             // TODO 1:
             // this currently fills the background with an obnoxious yellow;
             // you should modify both the height and color to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'gray');
             background.addChild(backgroundFill);
-            
+            var sky = draw.rect(canvasWidth, 380, "#031245", 8, 3);
+            background.addChild(sky);
             // TODO 2: - Add a moon and starfield
-            
-            
+            var moon = draw.circle(3, "pale", "white", 3);
+            background.addChild(moon);
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
             
