8a67a89917b36ab03eb6c9d47b1f0322a71e7d21
cchopper123 <c34504899@gmail.com>
2024-05-31 18:09:40 +0000

    saving

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -57,6 +57,10 @@ var background = function (window) {
             
             
             // TODO 3: Part 1 - Add a tree
+            var tree = draw.bitmap("img/tree.png")
+            tree.x=700;
+            tree.y=450;
+            background.addChild(tree);
             
             
         } // end of render function - DO NOT DELETE
@@ -71,7 +75,12 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            
+            function move_tree(){
+                tree.x = tree.x +1;
+                if (tree.x < -200){
+                    tree.x = canvasWidth;
+                }
+            }
             
             // TODO 4: Part 2 - Parallax
             
