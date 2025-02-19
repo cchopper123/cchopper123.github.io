1ed58126682a5f667e47d03ae34a82bc81717b8d
IoT <IoT@Supers-Mac-mini.local>
2025-02-18 19:42:19 -0600

    saving

--- iot-projects/are-you-being-served/fetchServer.js
+++ iot-projects/are-you-being-served/fetchServer.js
@@ -1,11 +1,23 @@
 // fetchServer.js file
+var args = process.argv.slice(2);
+
 const http = require("http");
 var port = 8080;
 
 http.createServer(async function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
-    console.log("raggedy princess could make you a new hambo in 10 seconds");
-    var fetchResponse = await fetch("https://github.com/cchopper123/cchopper123.github.io.git");
+    //console.log("raggedy princess could make you a new hambo in 10 seconds");
+    var url =args[0] ? args[0]:"<https://cchopper123.github.io>";
+    var fetchResponse = await fetch(url);
+    if (fetchResponse.ok===true){
+        var html = await fetchResponse.text();
+        res.write(html);
+        res.end();
+    }else{
+        res.write(fetchResponse.statusText, fetchResponse.status);
+        res.end();
+    }
+
 }).listen(port);
 //or raggedy princess could be your new hambo
 //she'd do it too
