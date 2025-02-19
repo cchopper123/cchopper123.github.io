6a35132f951e92c5c74c81a6f90c8f2333b78ef5
IoT <IoT@Supers-Mac-mini.local>
2025-02-18 19:11:29 -0600

    saving

--- iot-projects/are-you-being-served/fetchServer.js
+++ iot-projects/are-you-being-served/fetchServer.js
@@ -1,3 +1,17 @@
 // fetchServer.js file
 const http = require(http);
 const fetch = require(node-fetch);
+var port = 7979;
+function requestListenerFunction(){
+
+}
+http.createServer(requestListenerFunction).listen(port);
+http.createServer(async function(req, res) {
+//raggedy princess could make you a new hambo in 10 seconds
+}).listen(port);
+//or raggedy princess could be your new hambo
+//she'd do it too
+//that girl has 0 self respect
+//hah
+//that's mean
+//don't tell her I said that
