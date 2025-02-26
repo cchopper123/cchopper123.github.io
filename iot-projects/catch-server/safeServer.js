a88f6d22ad8f099804457c804baf4a2e13f3f441
IoT <IoT@Supers-Mac-mini.local>
2025-02-25 19:19:31 -0600

    saving

--- iot-projects/catch-server/safeServer.js
+++ iot-projects/catch-server/safeServer.js
@@ -3,14 +3,14 @@ const port=3000;
 let serverStatus = undefined;
 function requestListener(req, res){
     try{
-        if (req.method==="GET"){
+        if (req.method === "GET"){
             res.writeHead(200,{'Content-Type':'text/plain'});
             res.write(serverStatus);
         }
     }
-    catch{
-        res.write("Server no data");
+    catch(err) {
         res.writeHead(500, {"Content-Type":"text/json"});
+        res.write("Server no data");
     }
     finally{
         res.write(" - and the message arrived");
