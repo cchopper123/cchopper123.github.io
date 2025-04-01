82cdfde6c798c36e6bdcac02eb626af4ea1e57a9
IoT <IoT@supers-mac-mini.lan>
2025-04-01 17:41:02 -0500

    saing

--- iot-projects/catch-server/safeServer.js
+++ iot-projects/catch-server/safeServer.js
@@ -1,35 +1,31 @@
 const http = require("http");
-const port=3000;
+const port = 3000;
 let serverStatus = undefined;
-function requestListener(req, res){
-    try{
-        if (req.method === "GET"){
-            res.writeHead(200,{'Content-Type':'text/plain'});
-            res.write(serverStatus.message);
-        }
-        else if (req.method==="PUT"){
-            var body = "";
-            req.on("data", function (data){
-                body += data;
-            });
-            req.on("end", function(){
-                serverStatus={};
-                console.log(body)
-                serverStatus = JSON.parse(body);
-                res.writeHead(200, {'Content-Type':"text/plain"});
-            });   
-                         res.write("Server Updated");
-
-        }
+function requestListener(req, res) {
+  try {
+    if (req.method === "GET") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write(serverStatus.message);
+    } else if (req.method === "PUT") {
+      var body = "";
+      req.on("data", function (data) {
+        body += data;
+      });
+      req.on("end", function () {
+        serverStatus = {};
+        console.log(body);
+        serverStatus = JSON.parse(body);
+      });
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write("Server Updated");
     }
-    catch {
-        res.writeHead(500, {"Content-Type":"text/json"});
-        res.write("Server no data");
-    }
-    finally{
-        res.write(" - and the message arrived");
-        res.end();
-    }
+  } catch {
+    //res.writeHead(500, {"Content-Type":"text/json"});
+    res.write("Server no data");
+  } finally {
+    res.write(" - and the message arrived");
+    res.end();
+  }
 }
 
 const server = http.createServer(requestListener).listen(port);
