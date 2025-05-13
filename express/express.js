96812b52a03a7012c8a9791eb136dbbeefabae20
IoT <IoT@supers-mac-mini.lan>
2025-05-13 18:06:49 -0500

    saing

--- express/express.js
+++ express/express.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const json2html = require('node-jsonhtml');
 const app = express();
 const port = 3000;
 
+let data = {
+    'name':'cheese',
+    'age':3
+};
+
+let transform= [];
+let html = json2html(render(data, transform));
+console.log(html.split('><').join('>\n<'));
 
 const mid1 = (req,res, next) =>{
     req.first = "first"
@@ -13,11 +22,18 @@ const mid2 = (req,res, next) =>{
 }
 const mid3 = (req,res, next) =>{
     req.third = "third"
-    res.send(req.first + req.second + req.third);
+    res.send(`${req.first}  ${req.second}  ${req.third}  ${req.fourth}`);
 }
+const mid4 = (req,res, next) =>{
+    req.fourth = "fourth"
+    next();
+}
+
+
 app.use(mid1);
 app.use(mid2);
+app.get("/secret", mid4);
 app.use(mid3);
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`I am sad ${port}`)
 })
---
+++ express/json.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const json2html = require("node-jsonhtml");
+const app = express();
+const port = 3000;
+
+let data = [
+  { name: "Heat", age: "1996" },
+  { name: "Snatch", age: "2000" },
+];
+
+let transform = { "<>": "div", text: "${name} ${age}" };
+let html = json2html(render(data, transform));
+
+console.log(html.split("><").join(">\n<"));
+
+app.listen(port, () => {
+  console.log(`I am sad ${port}`);
+});
