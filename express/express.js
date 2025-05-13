b46dbd53821118b1752391190c1d0a2893773fdd
IoT <IoT@supers-mac-mini.lan>
2025-05-13 17:30:34 -0500

    sa

--- express/express.js
+++ express/express.js
@@ -1,16 +1,19 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 3000;
 
 
 const mid1 = (req,res, next) =>{
-
+    req.first = "first"
+    next()
 }
 const mid2 = (req,res, next) =>{
-    
+    req.second = "second"
+    next()
 }
 const mid3 = (req,res, next) =>{
-    
+    req.third = "third"
+    res.send(req.first + req.second + req.third);
 }
 app.use(mid1);
 app.use(mid2);
