1388a61fcde23f02d81560d7f2370766fd6af8c4
IoT <IoT@supers-mac-mini.lan>
2025-05-13 17:24:51 -0500

    saing

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,5 +1,11 @@
-const express = require('express'),
-	cors = require('cors');
-	
+const express = require("express"),
+  cors = require("cors");
 
+var app = express();
+app.use(cors());
+app.get("/", function (req, res) {
+  res.send("port");
+});
 module.exports = app;
+
+// I have looked through all files
