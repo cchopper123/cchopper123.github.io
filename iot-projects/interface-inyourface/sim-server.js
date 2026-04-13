e83476d69e53f75afc0bc1514e59e979dafb28b9
cchopper123 <c34504899@gmail.com>
2026-04-13 16:50:55 -0500

    saving

--- iot-projects/interface-inyourface/sim-server.js
+++ iot-projects/interface-inyourface/sim-server.js
@@ -29,7 +29,7 @@ const server = http.createServer(function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
+    "GET, POST, PUT, DELETE, OPTIONS",
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   /* DO NOT EDIT THIS CODE */
